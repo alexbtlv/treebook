4d137a1b56cd413cc3fda550422cb0d3f0f61303
Alexander Batalov <alexbtlv@gmail.com>
2014-05-14 00:39:32 +0600

    ok, sample galery thumbnail works

--- app/helpers/albums_helper.rb
+++ app/helpers/albums_helper.rb
@@ -1,2 +1,9 @@
 module AlbumsHelper
+	def album_thumbnail(album)
+		if album.pictures.count > 0
+			image_tag(album.pictures.first.asset.url(:small))
+		else
+			image_tag("http://www.rockthebells.net/wp-content/uploads/2013/08/kendrick-lamar-crown1.jpg")
+		end
+	end
 end
